649f2b83f201d99a77ce45217d10092d4f7cc361
John Nosek <john.nosek4@gmail.com>
2025-01-29 10:13:28 -0800

    up through 3.18

--- index.js
+++ index.js
@@ -46,12 +46,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (body.content === undefined) {
-        return response.status(400).json({ error: 'content missing' })
-    }
 
     const note = new Note({
         content: body.content,
@@ -60,19 +56,20 @@ app.post('/api/notes', (request, response) => {
 
     note.save().then(savedNote => {
         response.json(savedNote)
-    })
+    }).catch(error => next(error))
 })
 
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
+    const { content, important } = request.body
 
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+
+        { content, important },
+        { new: true, runValidators: true, context: 'query' }
+    )
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -93,10 +90,13 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
-
     next(error)
 }
+
+
 
 // this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(errorHandler)
